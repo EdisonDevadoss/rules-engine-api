efa760862e0e5619f42166387d572d215a53430a
ragu <ragu25@codingtown.com>
2020-03-06 18:43:15 +0530

    Remove schema

--- app/routes/ruleDetails.routes.js
+++ app/routes/ruleDetails.routes.js
@@ -1,8 +1,7 @@
-const { createRuleDetailSchema } = require('./ruleDetails.schema');
 const ruleDetailsController = require('../controllers/ruleDetails.controller');
 
 function ruleDetails(fastify, opts, next) {
-  fastify.post('/rule_details', createRuleDetailSchema, ruleDetailsController.create);
+  fastify.post('/rule_details', ruleDetailsController.create);
   fastify.get('/rule_details', ruleDetailsController.list);
   fastify.get('/rule_details/:id', ruleDetailsController.getById);
   fastify.put('/rule_details/:id', ruleDetailsController.update);
